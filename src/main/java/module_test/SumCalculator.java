63ad77ee0f5116fdba0ad29e88200be3b3375f20
ideathreject <fury.extreme1@gmail.com>
2025-04-30 13:16:54 +0200

    refactor

--- src/main/java/module_test/SumCalculator.java
+++ src/main/java/module_test/SumCalculator.java
@@ -2,7 +2,7 @@ package module_test;
 
 public class SumCalculator {
     public int sum(int n) {
-        if (n < 1 ) throw new IllegalArgumentException("The number should be > 1");
+        if (n < 1) throw new IllegalArgumentException("The number should be > 1");
         int result = 0;
         for (int i = 1; i <= n; i++) {
             result += i;
